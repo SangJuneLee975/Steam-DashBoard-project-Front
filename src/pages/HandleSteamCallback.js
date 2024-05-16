730f3a761efb4f322ab4ae5f90d961539f043c48
SangJuneLee975 <pkok975@gmail.com>
2024-05-16 23:10:18 +0900

    스팀계정과 연동 하는 로직 업데이트 중

--- src/pages/HandleSteamCallback.js
+++ src/pages/HandleSteamCallback.js
@@ -11,15 +11,16 @@ const HandleSteamCallback = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('accessToken');
-    const claimedId = params.get('claimedId');
+    //const claimedId = params.get('claimedId');
+    const redirectUrl = params.get('redirectUrl');
 
     if (accessToken) {
       localStorage.setItem('accessToken', accessToken);
       setIsLoggedIn(true);
       setAccessToken(accessToken);
-      navigate('/profile'); // 사용자를 프로필 페이지로 리다이렉션
+      navigate(redirectUrl || '/profile'); // 사용자를 프로필 페이지로 리다이렉션
     } else {
-      console.error('No access token in callback URL');
+      console.error('콜백 URL에 토큰이 없습니다');
     }
   }, [navigate, setIsLoggedIn, setAccessToken]);
 
