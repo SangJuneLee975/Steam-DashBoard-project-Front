d16a2be198fdf60dd90440dbcc635d7c8d79541b
SangJuneLee975 <pkok975@gmail.com>
2024-05-18 23:36:30 +0900

    스팀계정 연동 관련 업데이트 중

--- src/pages/HandleSteamCallback.js
+++ src/pages/HandleSteamCallback.js
@@ -11,18 +11,27 @@ const HandleSteamCallback = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const accessToken = params.get('accessToken');
-    //const claimedId = params.get('claimedId');
+    const claimedId = params.get('claimedId');
     const redirectUrl = params.get('redirectUrl');
     const steamNickname = params.get('steamNickname');
 
     if (accessToken) {
+      // JWT 토큰과 스팀 닉네임을  저장
       localStorage.setItem('accessToken', accessToken);
       if (steamNickname) {
         localStorage.setItem('steamNickname', steamNickname);
       }
+      // 로그인 상태와 액세스 토큰을 Recoil 상태로 설정
       setIsLoggedIn(true);
       setAccessToken(accessToken);
-      navigate(redirectUrl || '/'); // 홈으로 리다이렉트
+
+      // 스팀 ID 저장
+      if (claimedId) {
+        localStorage.setItem('steamId', claimedId);
+      }
+
+      // 홈으로 리다이렉트
+      navigate(redirectUrl || '/');
     } else {
       console.error('콜백 URL에 토큰이 없습니다');
     }
